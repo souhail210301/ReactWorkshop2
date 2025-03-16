60d80e10932179d4ab97ec0474a6575ea8227005
Souhail Abdennebi <100965022+souhail210301@users.noreply.github.com>
2025-03-16 16:30:41 +0100

    Update Favorites.jsx

--- src/UseCase/Favorites.jsx
+++ src/UseCase/Favorites.jsx
@@ -10,7 +10,6 @@ export default function Favorites() {
   return (
     <div>
       <h2>Mes Favoris</h2>
-      
       {favorites.length === 0 ? (
         <Alert variant="info">
           Aucun élément en favoris
@@ -21,8 +20,8 @@ export default function Favorites() {
             <Event 
               key={eventItem.id} 
               event={eventItem} 
-              fnt={() => {}} // Vous pouvez passer une fonction vide ou adapter selon votre besoin
-              deleteE={() => {}} // Vous pouvez passer une fonction vide ou adapter selon votre besoin
+              fnt={() => {}}
+              deleteE={() => {}}
             />
           ))}
         </Row>
